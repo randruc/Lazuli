0fc3c42e3ead2264e9fabaab9e77a5f36748c572
Remi Andruccioli <remi.andruccioli@gmail.com>
2017-01-31 23:33:47 +0100

    Trailing Whitespaces.

--- src/kern/main.c
+++ src/kern/main.c
@@ -29,22 +29,22 @@ void
 Main()
 {
   TimerCounter0 * const timer0 = GetTimerCounter0();
-  
+
   DDRB = (u8)0xff;
   EICRA = (u8)0x03;
   EIMSK = (u8)0x01;
 
   mode = true;
-  
+
   timer0->tccr0b = 0;
   timer0->tccr0b = TCCR0B_CS02 | TCCR0B_CS00;
   timer0->tcnt0 = 0;
 
   TimerCounter0InterruptsEnable(TIMSK0_TOEI0);
   GlobalInterruptsEnable();
-  
+
   PORTB = u8_MIN;
-  
+
   while (true) {
     if (mode) {
       PORTB = u8_MAX;
