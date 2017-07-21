98db86e707c49ae1c716e54fdf4351f13843efe6
Remi Andruccioli <remi.andruccioli@gmail.com>
2017-07-21 21:46:55 +0200

    _EXTERN_C_DECL_BEGIN and _EXTERN_C_DECL_END were missing in include/Lazuli/sys/scheduler.h.

--- src/include/Lazuli/sys/scheduler.h
+++ src/include/Lazuli/sys/scheduler.h
@@ -7,10 +7,16 @@
 #ifndef LZ_SYS_SCHEDULER_H
 #define LZ_SYS_SCHEDULER_H
 
+#include <Lazuli/common.h>
+
+_EXTERN_C_DECL_BEGIN
+
 /**
  * Initialize the scheduler.
  */
 void
 Scheduler_Init();
+
+_EXTERN_C_DECL_END
 
 #endif /* LZ_SYS_SCHEDULER_H */
