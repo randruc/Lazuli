d4fe9a717a27e40c2bdbc151a49619f1828f1e16
Remi Andruccioli <remi.andruccioli@gmail.com>
2018-07-04 22:57:27 +0200

    Reordered #includes in schduler_rr.c.

--- src/kern/scheduler_rr.c
+++ src/kern/scheduler_rr.c
@@ -11,8 +11,8 @@
 #include <Lazuli/lazuli.h>
 
 #include <Lazuli/sys/arch/arch.h>
-#include <Lazuli/sys/arch/AVR/timer_counter_0.h>
 #include <Lazuli/sys/arch/AVR/interrupts.h>
+#include <Lazuli/sys/arch/AVR/timer_counter_0.h>
 #include <Lazuli/sys/kernel.h>
 #include <Lazuli/sys/list.h>
 #include <Lazuli/sys/scheduler_base.h>
