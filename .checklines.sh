8b4ab6aa7d4fce6266950dce6ff60b607226d296
Remi Andruccioli <remi.andruccioli@gmail.com>
2020-08-25 21:12:03 +0200

    checklines.sh: Specify grep include file patterns for each kind of check.

--- .checklines.sh
+++ .checklines.sh
@@ -1,33 +1,74 @@
 #!/bin/bash
 
-filetypes="--include=*.c \
-     --include=*.h \
-     --include=*.sh \
-     --include=*.ld \
-     --include=*.S \
-     --include=*.txt \
-     --include=*.dox \
-     --exclude-dir=build \
-     --exclude-dir=_build"
+#
+# Bash script to perform tests on lines of text.
+#
+# Include patterns for grep are sorted alphabetically.
+#
 
 failed=0
 
 # Print lines longer than 80 characters
 ! grep -nrH \
-     '.\{81\}'\
-     --color \
-     $filetypes || failed=1
+  '.\{81\}'\
+  --color \
+  --include=*.c \
+  --include=*.dox \
+  --include=*.h \
+  --include=*.ld \
+  --include=*.S \
+  --include=*.sh \
+  --include=*.txt \
+  --exclude-dir=build \
+  --exclude-dir=_build || failed=1
 
 # Find tab characters
 ! grep -nrHP \
-     --color \
-     "\t" \
-     $filetypes || failed=1
+  --color \
+  "\t" \
+  --include=*.c \
+  --include=*.cmake \
+  --include=*.dox \
+  --include=*.h \
+  --include=*.ld \
+  --include=*.md \
+  --include=*.rst \
+  --include=*.S \
+  --include=*.sh \
+  --include=*.txt \
+  --include=*.yml \
+  --exclude-dir=build \
+  --exclude-dir=_build || failed=1
 
 # Find trailing whitespaces
 ! grep -nrH \
-     "\([[:space:]]\)$" \
-     --color \
-     $filetypes || failed=1
+  "\([[:space:]]\)$" \
+  --color \
+  --include=*.c \
+  --include=*.cmake \
+  --include=*.dox \
+  --include=*.h \
+  --include=*.ld \
+  --include=*.S \
+  --include=*.sh \
+  --include=*.txt \
+  --include=*.yml \
+  --exclude-dir=build \
+  --exclude-dir=_build || failed=1
+
+# Find \r characters
+# ! grep -nrHP \
+#      "\r" \
+#      --color \
+#      --include=*.c \
+#      --include=*.dox \
+#      --include=*.h \
+#      --include=*.ld \
+#      --include=*.rst \
+#      --include=*.S \
+#      --include=*.sh \
+#      --include=*.txt \
+#      --exclude-dir=build \
+#      --exclude-dir=_build || failed=1
 
 exit $failed
