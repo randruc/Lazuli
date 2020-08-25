d31a215cf821b84804cbadf5169711d6c4ccc6a9
Remi Andruccioli <remi.andruccioli@gmail.com>
2020-08-25 21:17:16 +0200

    stack_usage.sh: Improvements on the usability.
    * Command line options have been added to let the user sort by stack size or by
      file name.
    * The output is no longer piped into less.

--- stack_usage.sh
+++ stack_usage.sh
@@ -1,8 +1,24 @@
-#
-# Simple bash one liner to display the stack usage of the whole project nicely
-# presented and sorted.
-#
-
 #!/bin/bash
 
-find . -type f -name *.su | xargs cat | column -td | sort -rn -k2 | less
+#
+# Bash script to display the stack usage of the whole project nicely presented
+# and sorted.
+#
+
+case $1 in
+    -h)
+        echo "$0 usage:"
+        echo "    -h  Display help"
+        echo "    -n  Order by file name (default)"
+        echo "    -s  Order by stack size"
+        ;;
+
+    -s)
+        find . -type f -name *.su | xargs cat | column -td | sort -rn -k2
+        ;;
+
+    -n|*)
+        find . -type f -name *.su | xargs cat | column -td | sort -k1
+        ;;
+esac
+
