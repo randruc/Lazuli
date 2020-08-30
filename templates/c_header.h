3f710b866e3a8fe889fcfb218ed2c9df36586210
Remi Andruccioli <remi.andruccioli@gmail.com>
2020-08-30 15:36:09 +0200

    C files templates: Introduce templates to be used for new C files.
    These tempates MUST be used to create new C files for the project.

---
+++ templates/c_header.h
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * This file is part of Lazuli.
+ */
+
+/**
+ * @file
+ * @brief
+ * @copyright
+ *
+ *
+ */
+
+#ifndef
+#define
+
+#include <Lazuli/common.h>
+
+_EXTERN_C_DECL_BEGIN
+
+_EXTERN_C_DECL_END
+
+#endif /*  */
---
+++ templates/c_source.c
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * This file is part of Lazuli.
+ */
+
+/**
+ * @file
+ * @brief
+ * @copyright
+ *
+ *
+ */
